ffd04fdf925dfaca4ed7bb96520b7d3fb1cd7d05
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2022-12-16 20:30:54 +0700

    update program

--- dz/Program.cs
+++ dz/Program.cs
@@ -1,5 +1,22 @@
-﻿
-Console.WriteLine("Введите число ");
-int a = Convert.ToInt32(Console.ReadLine());
-a = (a / 10) % 10;
-Console.WriteLine(a);
+public static class Lesson456
+{
+    static void Main()
+    {
+
+      //Определение количества вводимых чисел
+
+      Console.Write("Введите количество чисел ");
+      int A = int.Parse(Console.ReadLine());
+      int count = 0;
+      for (int i = 0; i < A; i++)
+    {
+      //Введение чисел пользователем
+      Console.Write($"Введите число {i + 1}: ");
+      int B = int.Parse(Console.ReadLine());
+
+      //Вычисление количества положительных чисел
+
+      if (B > 0)
+        count++;
+      Console.WriteLine($"Положительных чисел: {count}");
+}}  }
